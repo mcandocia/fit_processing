6bafacb080c4e94720b25cf598473154f9efc79d
Max Candocia <mscandocia@gmail.com>
2020-02-12 17:25:27 -0600

    added tz update

--- import_and_process_garmin_fit.py
+++ import_and_process_garmin_fit.py
@@ -22,14 +22,20 @@ def main(
     os.makedirs(fit_target_dir, exist_ok=True)
     os.makedirs(fit_processed_csv_dir, exist_ok=True)
     activity_files = os.listdir(fit_source_dir)
+    print('activity files: ', activity_files)
     new_names = [FNAME_REGEX.sub('.fit', file) for file in activity_files]
+    print('new names: ', new_names)
     current_files = set(os.listdir(fit_target_dir))
+    print('current_files: ', current_files)
     for src_file, tgt_file in zip(activity_files, new_names):
-        if tgt_file in current_files:
+        if FNAME_REGEX.sub('.fit', tgt_file) in current_files:
+            print('%s already exists...' % tgt_file)
             continue
+        else:
+            pass
         shutil.copyfile(
             os.path.join(fit_source_dir, src_file),
-            os.path.join(fit_target_dir, src_file)
+            os.path.join(fit_target_dir, tgt_file)
         )
         print("copied %s to %s" % (
             os.path.join(fit_source_dir, src_file),
